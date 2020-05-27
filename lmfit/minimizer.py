7eca92c9151a274db8dcf3ac039a25500b963514
Varvara Efremova <varvara@echus.co>
2020-05-27 18:26:35 +1000

    Test

--- lmfit/minimizer.py
+++ lmfit/minimizer.py
@@ -684,9 +684,9 @@ class Minimizer:
         if params is not None:
             self.params = params
         if isinstance(self.params, Parameters):
-            print("prepare_fit params.attrs:", getattr(params, 'attrs'))
-            print("prepare_fit after deepcopy params.attrs:", getattr(params, 'attrs'))
+            print("prepare_fit params.attrs:", getattr(self.params, 'attrs'))
             result.params = deepcopy(self.params)
+            print("prepare_fit after deepcopy params.attrs:", getattr(result.params, 'attrs'))
         elif isinstance(self.params, (list, tuple)):
             result.params = Parameters()
             for par in self.params:
